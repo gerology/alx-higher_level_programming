501b9570484e916f45a19e8f8cb046d76c905475
gerology <gerology007@gmail.com>
2022-12-01 12:59:18 +0000

    print the last digit of a number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import random
-from math import floor
+import sys
 number = random.randint(-10000, 10000)
 
 if number < 0:
@@ -12,15 +12,16 @@ else:
 def last(n):
     """determine the state of a number with conditions"""
     if n > 5:
-        greater = 'and is greater than 5'
+        greater = "and is greater than 5"
         return greater
     elif n == 0:
-        equalto0 = 'and is 0'
+        equalto0 = "and is 0"
         return equalto0
-    elif n < 6 & n != 0:
-        less6 = 'and is less than 6 and not 0'
+    elif n < 6 and n != 0:
+        less6 = "and is less than 6 and not 0"
         return less6
-
+    else:
+        sys.stderr.write(invalid)
 
 stmt = last(lastNum)
 print("Last digit of {0} is {1} {2}".format(number, lastNum, stmt))
