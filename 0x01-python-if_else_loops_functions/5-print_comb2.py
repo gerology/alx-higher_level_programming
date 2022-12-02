38d3ca1779b1ac7e57771b790d0160491ee2556c
gerology <gerology007@gmail.com>
2022-12-02 21:15:01 +0000

    print number 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for x in range(0, 100):
-    print("{:02}".format(x), end=", ")
     if x == 99:
-        print("{}".format(x))
+        print("{}".format(x), end="")
+    else:
+        print("{:02}".format(x), end=", ")
