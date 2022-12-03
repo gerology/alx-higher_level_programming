46fb60edd8d6e0242037af94ddfe26ace8cf100b
gerology <gerology007@gmail.com>
2022-12-03 08:11:19 +0000

    print strings in upperscase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,6 @@ def uppercase(str):
     for x in str:
         num = ord(x)
         if num >= 97 and num <= 122:
-            print("{}".format(chr(num - 32)), end="")
-        else:
-            print("{}".format(chr(num)), end="")
+            num = num - 32
+        print("{}".format(chr(num)), end="")
     print()
