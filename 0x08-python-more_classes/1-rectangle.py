3c72de895113e481f1a4eff80b72961a62c49016
gerology <gerology007@gmail.com>
2023-01-04 18:01:48 +0000

    Rectangle class height and width attributes

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+
+"""
+ Rectangle class defines rectangle by width and height based on 0-rectangle.py
+"""
+
+
+class Rectangle:
+
+    def __init__(self, width=0, height=0):
+        """ initializes the data"""
+
+        self.__width = width
+        self.__height = height
+
+
+    def set_width(self, value):
+        """ set width value """
+
+        self.__value = value
+
+        if type(self.__width) != int:
+            raise TypeError("width must be an integer")
+
+        if self.__width < 0:
+            raise ValueError("with must be >=0")
+
+    def get_height(self):
+        """ retrieve the height"""
+
+        return self.__height
+
+    def set_height(self, value):
+        """ retrieve the value of height"""
+
+        self.__value = value
+
+        if type(self.__height) == int:
+            raise TypeError("height must be an integer")
+
+        if (self.__height) < 0:
+            raise ValueError("height must be >= 0")
