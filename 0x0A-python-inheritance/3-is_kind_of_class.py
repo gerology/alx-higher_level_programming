aced8fbb0c71c321e75d7582a2a80e89efc94b77
gerology <gerology007@gmail.com>
2023-01-10 10:55:39 +0000

    returns true object is an instance of class inherited from

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,21 +3,22 @@
 """ determine the object is an instance
      of a class or a class inherited from specified class
 """
+
+
 def is_kind_of_class(obj, a_class):
     """
-        returns True if the ibject is an instance 
+        returns True if the ibject is an instance
         of the specified class
 
         Args:
            obj - object to check
            a_class - class to check objwct against
 
-        rsturn:
-           return True if object is an instance of 
+        return:
+           return True if object is an instance of
            specified class
            else returns False
     """
     if isinstance(obj, a_class):
         return True
     return False
-
