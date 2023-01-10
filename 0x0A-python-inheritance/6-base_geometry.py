f9164c8a8a7bf1eb16f04ec81c075031ee83e0e9
gerology <gerology007@gmail.com>
2023-01-10 10:36:45 +0000

    BaseGeometry class with public instance 'def area(self)' that raises exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" define a class 'BaseGeometry' """
+
+class BaseGeometry:
+    """ representation of BaseGeometry"""
+
+    def area(self):
+        """ raise exception if not implemented """
+        raise Exception("area() is not implemented")
