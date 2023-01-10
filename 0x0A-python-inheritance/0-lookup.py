f5217434686f175fb5ec97b05f791a8b4a72433c
gerology <gerology007@gmail.com>
2023-01-10 03:34:22 +0000

    return a list, and a class that inherits list

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,9 +4,7 @@
 
 
 def lookup(obj):
-    """ method that returns a 
+    """ method that returns a
        list of attributes of object
     """
-    def __init__(self):
-        """ new instance ob object"""
-        return (dir(obj))
+    return (dir(obj))
--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
-
 """ define a model that inherits from a list"""
-def print_sorted(self):
-    """ print the list in ascending order"""
-    print(sorted(self))
+
+
+class MyList(list):
+    """ sorted printing """
+    def print_sorted(self):
+        """ print the list in ascending order"""
+        print(sorted(self))
