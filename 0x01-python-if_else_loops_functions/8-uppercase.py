afa4dadba494ee7e42db3ff67b4bd683ae9c237d
gerology <gerology007@gmail.com>
2023-02-16 21:25:34 +0000

    updates and corrections

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(str):
     for x in str:
         if ord(x) >= 97 and ord(x) <= 122:
             x = chr(ord(x) - 32)
-        print("{}".format(x), end="")
-    print("")
+        print(f"{x}")
+    print()
