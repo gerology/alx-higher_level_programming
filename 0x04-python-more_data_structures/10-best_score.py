f471a852c953c9074cad00529545d063a86450c6
gerology <gerology007@gmail.com>
2023-02-27 16:25:32 +0000

    return the key of the biggest integer valu

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if not isinstance(a_dictionary, dict) or len(a    _dictionary) == 0:
+        return None
+
+    g = list(a_dictionary.keys())[0]
+    s = 0
+    b = a_dictionary[g]
+    for k, v in a_dictionary.items():
+        if v > b:
+            b = v
+            g = k
+    return (g)
