757449cb50d062585e20ee3c4e11d90dcbaa1203
gerology <gerology007@gmail.com>
2023-03-01 23:27:12 +0000

    correction update

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    for g in reversed(my_list):
-        print("{:d}".format(g))
+    if isinstance(my_list, list):
+        my_list.reverse()
+        for g in my_list:
+            print("{:d}".format(g))
