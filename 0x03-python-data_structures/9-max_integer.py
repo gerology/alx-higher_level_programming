5ee7e2d68374debd24db7ae70f56b6f0215206ea
gerology <gerology007@gmail.com>
2023-03-02 00:24:30 +0000

    correction update

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,8 +3,8 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    sum = 0
-    for i in my_list:
+    sum = my_list[0]
+    for i in my_list[1:]:
         if i > sum:
             sum = i
     return (sum)
