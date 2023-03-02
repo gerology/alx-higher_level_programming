a1400dce6c3ca4f4f3e68d0cbe6ed309ae7cf53c
gerology <gerology007@gmail.com>
2023-03-02 06:12:32 +0000

    corrections update

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    l = len(my_list)
     new_list = []
     for item in my_list:
-        if item % 2 ==  0 and item <= l:
+        if item % 2 == 0 and item <= len(my_list):
             new_list.append(True)
         else:
             new_list.append(False)
