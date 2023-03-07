ac5d46adb467805f74b7a02aa58f18e8ba79c7ad
gerology <gerology007@gmail.com>
2023-03-07 13:09:28 +0000

    corrections

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" creates a rectangle"""
+
+""" model for a Rectangle """
 
 
 class Rectangle:
@@ -16,15 +17,17 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-        type(self).number_of_instances += 1
 
     @property
     def width(self):
-        """retrieve the width"""
+        """ get the width of triangle"""
+
         return self.__width
 
     @width.setter
     def width(self, value):
+        """set width for rectangle """
+
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -50,8 +53,8 @@ class Rectangle:
 
     def area(self):
         """return the area of a rectangle"""
-        self.area = self.__width * self.__height
-        return self.area
+        area = self.__width * self.__height
+        return (area)
 
     def perimeter(self):
         """ return the perimeter of a rectangle"""
@@ -61,6 +64,16 @@ class Rectangle:
             return ("")
         self.perimeter = (self.__width * 2) + (self.__height * 2)
         return self. perimeter
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return (rect_1)
+        return(rect_2)
 
     def __str__(self):
         rep = []
