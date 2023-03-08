3185805f56d7f435bee25e807bb67462fb9dc516
gerology <gerology007@gmail.com>
2023-03-08 12:32:40 +0000

    python inputs and outputs

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,4 @@
 def read_file(filename=""):
     """ read text file and output to stdout"""
     with open(filename, encoding="utf-8") as f:
-        read = f.read()
-    print(read)
+    print(f.read(), end="")
