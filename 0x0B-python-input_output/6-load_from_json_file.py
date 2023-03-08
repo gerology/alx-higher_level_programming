90c3b3c38b29a0adfd9a2568821e2e02162b1668
gerology <gerology007@gmail.com>
2023-03-08 13:24:12 +0000

    input and output

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     """Create a Python object from a JSON file."""
-    with open(filename, 'r') as f:
+    with open(filename) as f:
         return json.load(f)
