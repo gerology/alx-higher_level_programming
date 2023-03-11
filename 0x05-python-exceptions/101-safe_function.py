a84b1b85b26a68cfcd9314962a44fb3528d7a2ab
gerology <gerology007@gmail.com>
2023-03-11 06:39:21 +0000

    Exceptions

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,7 @@ import sys
 def safe_function(fct, *args):
     try:
         f = fct(*args)
-    except:
+    except Exception:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
     return (f)
