5ee29f42de2c9a80c2cff9322453bf101ca687c3
gerology <gerology007@gmail.com>
2023-03-14 13:28:52 +0000

    javascript

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const v = process.arv;
+const x = length(v);
+if (x < 1) {
+console.log("No argument");
+} else if (x === 1){
+console.log("Argument found");
+} else {
+console.log("Arguments found");
+})
