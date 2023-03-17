c8f3f651863c87295ec9daaef30b954bf5688f06
gerology <gerology007@gmail.com>
2023-03-17 13:07:07 +0000

    search second biggest number

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+if (process.argv.length <= 3) {
+  return 0;
+} else {
+  const val = process.argv.map(Number)
+  .slice(2, process.argv.lenth)
+  .sort((a, b) => a -b);
+  console.log(val[val.length - 2]);
+}
