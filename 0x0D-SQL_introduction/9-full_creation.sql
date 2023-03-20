7b28ebf53b5e3a5cb5cafbac2d475da3e72717e4
gerology <gerology007@gmail.com>
2023-03-20 23:06:26 +0000

    create second_table and add multiple rows

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,4 @@
 -- create a table 'second_table' in hbtn_0c_0 database
 CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
-INSERT INTO `second_table` VALUES(1, 'john', 10);
-INSERT INTO `second_table` VALUES(2, 'Alex', 3);
-INSERT INTO `second_table` VALUES(3, 'Bob', 14);
-INSERT INTO `second_table` VALUES(4, 'George', 8);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(1, 'john', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
 
