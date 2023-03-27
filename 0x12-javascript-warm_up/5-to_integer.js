ebf2e34d40d9dd6f9569effc1dfe6413d1b4969c
gerology <gerology007@gmail.com>
2023-03-27 19:33:49 +0000

    corrections

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
 const numb = Math.floor(Number(process.argv[2]));
-console.log(isNaN(numb) ? 'Not a number' : 'My number: ${numb}');
+console.log(isNaN(numb) ? 'Not a number' : `My number: ${numb}`);
