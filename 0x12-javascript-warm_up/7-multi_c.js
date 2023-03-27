9173d0fb65e34ecc1e498d87607ebc859bb64fa7
gerology <gerology007@gmail.com>
2023-03-27 20:17:21 +0000

    corrections

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const cas = Math.floor(Number(process.argv[2]));
 if (isNaN(cas)) {
-  console.log('Missing number of occurrences');} else {
+  console.log('Missing number of occurrences');
+} else {
   for (let i = 0; i < cas; i++) {
     console.log('C is fun');
   }
