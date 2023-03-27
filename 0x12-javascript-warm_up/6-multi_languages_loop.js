9449e1ffc1826796959d023b9d6bf6991eedd25e
gerology <gerology007@gmail.com>
2023-03-27 19:40:34 +0000

    corrections

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 
 const lst = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i < lst.length ; i++) {
-console.log(lst[i]);
+for (let i = 0; i < lst.length; i++) {
+  console.log(lst[i]);
 }
