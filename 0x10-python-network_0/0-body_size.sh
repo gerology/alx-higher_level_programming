0d58cc73fd8b13fa1bab314979e3f5f9c271ae7c
gerology <gerology007@gmail.com>
2023-04-04 13:33:13 +0000

    size of the response body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# bash script that takes in a url and displays size of the body of the response
-curl -s "$1" | wc -c
+ bash script that takes in a url and displays size of the body of the response
+curl -s -X GET "$1" | wc -c
