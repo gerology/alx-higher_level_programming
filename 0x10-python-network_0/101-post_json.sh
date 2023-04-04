377f2b85806d57d4051294cc1d3887aa41ffbf50
gerology <gerology007@gmail.com>
2023-04-04 15:09:31 +0000

    display body of response using JSON POST

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a JSON POST request to a URL passed as the first argument, and display the body of the response
-curl -s -H "Content-Type = application/json" -d "$(cat "$2")" "$1"
+curl -s -H "Content-Type=application/json" -d "$(cat "$2")" "$1"
