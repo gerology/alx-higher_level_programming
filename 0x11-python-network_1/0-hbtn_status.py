d014e74ac41a6a897b519dfde615e0155757accd
gerology <gerology007@gmail.com>
2023-04-08 12:08:43 +0000

    fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,11 @@
 import urllib.request
 
 
-respond = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-with urllib.request.urlopen(respond) as fyl:
-    resp = fyl.read()
-    print("Body response:")
-    print("\t - type: {}".format(type(resp)))
-    print("\t - content: {}".format(resp))
-    print("\t - utf8 content: {}".format(resp.decode("utf-8")))
+if __name__ == "__main__":
+    respond = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(respond) as fyl:
+        resp = fyl.read()
+        print("Body response:")
+        print("\t - type: {}".format(type(resp)))
+        print("\t - content: {}".format(resp))
+        print("\t - utf8 content: {}".format(resp.decode("utf-8")))
