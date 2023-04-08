aa81167216d549363caad9229a242797139823bd
gerology <gerology007@gmail.com>
2023-04-08 21:34:23 +0000

    send a POST request to the passed URL with the email as a parameter, and display the body of the response (decoded in utf-8)

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" takes an email and url and send a 
+""" takes an email and url and send a
     post requestwith email as parameter
       Usage: ./2-post_email.py <URL> <email>
 """
@@ -13,8 +13,7 @@ if __name__ == "__main__":
     val = {"email": sys.argv[2]}
     dat = urllib.parse.urlencode(val)
     dat = dat.encode("ascii")
-    
+
     sett = urllib.request.Request(url, dat)
     with urllib.request.urlopen(sett) as fyl:
         print(fyl.read().decode("utf-8"))
-
