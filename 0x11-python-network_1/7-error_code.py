c4130319b3e1c506a6584e3ba813fb2884714001
gerology <gerology007@gmail.com>
2023-04-08 22:22:58 +0000

    sends a request to the URL and displays the body of the response

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+
+#!/usr/bin/python3
+""" display the body of response for request sent to URL"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    sett = request.get(url)
+    if sett.status_code >= 400:
+        print("Error code: {}".format(sett.raise_for_status()))
+    else:
+        print(sett.text)
