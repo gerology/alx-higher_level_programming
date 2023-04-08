89ef2279400a0ddc4009b747c887b34147c4cda5
gerology <gerology007@gmail.com>
2023-04-08 12:02:21 +0000

    fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" fetch https://alx-intranet.hbtn.io/status """
+import urllib.request
+
+
+respond = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+with urllib.request.urlopen(respond) as fyl:
+    resp = fyl.read()
+    print("Body response:")
+    print("\t - type: {}".format(type(resp)))
+    print("\t - content: {}".format(resp))
+    print("\t - utf8 content: {}".format(resp.decode("utf-8")))
