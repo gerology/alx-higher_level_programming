542728e4144106252ce36425e4b3c5b85e23deb0
gerology <gerology007@gmail.com>
2023-04-08 22:01:07 +0000

    fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,6 +6,7 @@ import requests
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     sett = requests.get(url)
+    fyl = sett.read()
     print("Body response:")
     print("\t- type: {}".format(type(fyl.text)))
     print("\t- content: {}".format(fyl.text))
