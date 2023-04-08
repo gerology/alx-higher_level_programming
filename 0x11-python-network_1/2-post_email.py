f8bd604818fd07cf7ad70a6476a5f273521f6dde
gerology <gerology007@gmail.com>
2023-04-08 12:25:53 +0000

    takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" takes an email and url and send a post requestwith email as parameter"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ = "__main__":
+    url = sys.argv[1]
+    val = {"email": sys.argv[2]}
+    dat = urllib.parse(val).encode('ascii')
+    
+    sett = urllib.request.Request(url, dat)
+    with urllib.request.url(sett) as fyl:
+        print(fyl.read().decode("utf-8"))
+
