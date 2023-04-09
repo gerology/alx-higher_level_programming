acf7499741436dad7c3469d3f6918a80846f1bb8
gerology <gerology007@gmail.com>
2023-04-09 18:04:18 +0000

    print commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,6 +11,8 @@ if __name__ == "__main__":
     comit = sett.json()
     try:
         for k in range(10):
-            print("{}: {}".format(comit[k].get("sha"), comit[k].get("commit").get("author").get("name")))
+            print("{}: {}".format(
+                comit[k].get("sha"),
+                comit[k].get("commit").get("author").get("name")))
     except IndexError:
         pass
