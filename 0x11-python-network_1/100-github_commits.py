b45d1000764a0a4df598c4e8af0635e2784b5e2e
gerology <gerology007@gmail.com>
2023-04-10 16:27:38 +0000

    print commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,7 +6,8 @@ import sys
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2],
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+            sys.argv[2],
             sys.argv[1])
     sett = requests.get(url)
     comit = sett.json()
