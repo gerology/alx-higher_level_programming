f2a019b0613a4aada27de0d29871bdb11b603c28
gerology <gerology007@gmail.com>
2023-05-08 12:20:37 +0000

    web jquery

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,6 +1,6 @@
 $('document').ready(function () {
 	let langcode = $.param({ lang: $("INPUT#language_code").val() });
-	const url = "https://www.fourtonfish.com/hellosalut/";
+	const url = "https://www.fourtonfish.com/hellosalut/?";
 	$("INPUT#btn_translate").on("click", function () {
 		$.get(url + langcode, function (data) {
 			let trans = data.hello;
