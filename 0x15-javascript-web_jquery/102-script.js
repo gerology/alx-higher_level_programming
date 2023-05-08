2713e286dd6ab6aaf742d806d18554fd6d05fb29
gerology <gerology007@gmail.com>
2023-05-08 11:57:03 +0000

    web jquery

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,7 +1,10 @@
-$.get("https://www.fourtonfish.com/hellosalut/hello/", fuction (data) {
-	 $("INPUT#btn_translate").on("click", function () {
-		 let langcode = $("INPUT#language_code").val();
-		 let trans = data.hello.langcode;
-		$("DIV#hello").text(trans);
+$('document').ready(function () {
+	let langcode = $("INPUT#language_code").val();
+	const url = "https://www.fourtonfish.com/hellosalut/hello/";
+	$.get(url + langcode, fuction (data) {
+		$("INPUT#btn_translate").on("click", function () {
+			let trans = data.hello;
+			$("DIV#hello").text(trans);
+		});
 	});
 });
