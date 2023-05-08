98261860bbcecb92db5bea9c9c1130bf14d3ea1e
gerology <gerology007@gmail.com>
2023-05-08 07:01:44 +0000

    web jquery

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,9 +1,9 @@
 $.get("https://swapi-api.alx-tools.com/api/films/?format=json", function(muvi) {
 	let movie = muvi.result;
-	movititle = movie.map(function(mov) {
+	let movititle = movie.map(function(mov) {
 		return mov.title;
 	});
-	$.each(movititle, function(title) {
+	$.each(movititle, function(index, title) {
 		let lsit = $("<li></li>").text(title);
 		$("#list_movies").append(lsit);
 	});
