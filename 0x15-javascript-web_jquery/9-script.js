a3a6e022865ee49ed72cc33a1e4822d05987d1a1
gerology <gerology007@gmail.com>
2023-05-08 09:02:30 +0000

    web jquery

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,3 +1,5 @@
-$.get("https://fourtonfish.com/hellosalut/?lang=fr", function (data) {
+$("document").ready(function () {
+	$.get("https://fourtonfish.com/hellosalut/?lang=fr", function (data) {
 	$('#hello').text(data.hello);
+	});
 });
